2078313972c25b5afb26f2fd471250919c1daa87
manas <p3manas@gmail.com>
2015-05-17 23:48:01 +0530

    custim exception

--- src/main/java/com/mrptech/giveLife/util/ApplicationException.java
+++ src/main/java/com/mrptech/giveLife/util/ApplicationException.java
@@ -22,5 +22,17 @@ public class ApplicationException extends Exception {
 		super(name);
 	}
 
-	
+	public ApplicationException(Throwable cause){
+		super(cause);
+	}
+
+	public ApplicationException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public ApplicationException(String message, Throwable cause,
+	boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
 }
